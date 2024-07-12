10a165470e2fa46216595bc600b3bf4ddd0dbe33
rhwjdgns <167050760+rhwjdgns@users.noreply.github.com>
2024-07-12 22:08:49 +0900

    🎉 add : 핸들러, 매핑 추가

--- server_src/handlers/handlerMapping.js
+++ server_src/handlers/handlerMapping.js
@@ -1,11 +1,24 @@
 const { PacketType } = require('../constants');
-const { handleLogin, handleGameEnd, handleGameSync, handleGameOver } = require('./handlers');
+const {
+  handleLogin,
+  handleGameEnd,
+  handleGameSync,
+  handleGameOver,
+  handleDieMonster,
+  handleEnemyDieMonster,
+  handleSpawnMonster,
+  handleEnemySpawnMonster,
+} = require('./handlers');
 
 const handlerMapping = {
   [PacketType.C2S_LOGIN_REQUEST]: handleLogin,
   [PacketType.C2S_GAME_END_REQUEST]: handleGameEnd,
   [PacketType.C2S_GAMESYNC_REQUEST]: handleGameSync,
   [PacketType.S2C_GAME_OVER_NOTIFICATION]: handleGameOver,
+  [PacketType.C2S_SPAWN_MONSTER]: handleSpawnMonster,
+  [PacketType.S2C_ENEMY_SPAWN_MONSTER]: handleEnemySpawnMonster,
+  [PacketType.S2C_ENEMY_DIE_MONSTER]: handleEnemyDieMonster,
+  [PacketType.C2S_DIE_MONSTER]: handleDieMonster,
   // 추가 핸들러 매핑
 };
 
--- server_src/handlers/monster.handler.js
+++ server_src/handlers/monster.handler.js
@@ -1,45 +1,78 @@
-import { createMonsters, getMonsters, setMonster } from '../models/monster.model.js';
+const { sendGameSync } = require('./gameSyncHandler');
+const { PacketType } = require('../constants');
+const { createMonsters, getMonsters, setMonster } = require('../models/monster.model.js');
 
 // 아군 몬스터 사망
-export const DieMonster = (uuid, payload) => {
+function handleDieMonster(uuid, payload) {
   const monsters = getMonsters(uuid);
   if (!monsters) {
     return { status: 'fail', message: 'Monsters not found' };
   }
 
   setMonster(uuid, payload.monsterId, payload.monsterLevel);
+  sendGameSync(uuid, {
+    packetType: PacketType.C2S_DIE_MONSTER,
+    monsterId: payload.monsterId,
+    monsterLevel: payload.monsterLevel,
+  });
+
   return { status: 'success', message: 'Monster is dead' };
-};
+}
 
 // 적군 몬스터 사망
-export const EnemyDieMonster = (uuid, payload) => {
+function handleEnemyDieMonster(uuid, payload) {
   const monsters = getMonsters(uuid);
   if (!monsters) {
-    return { status: 'fall', message: 'Monsters not found' };
+    return { status: 'fail', message: 'Monsters not found' };
   }
 
   setMonster(uuid, payload.monsterId, payload.monsterLevel);
+  sendGameSync(uuid, {
+    packetType: PacketType.S2C_ENEMY_DIE_MONSTER,
+    monsterId: payload.monsterId,
+    monsterLevel: payload.monsterLevel,
+  });
+
   return { status: 'success', message: 'Monster is dead' };
-};
+}
 
 // 아군 몬스터 생성
-export const SpawnMonster = (uuid, payload) => {
+function handleSpawnMonster(uuid, payload) {
   const monsters = createMonsters(uuid);
   if (!monsters) {
     return { status: 'fail', message: 'Monsters not found' };
   }
 
   createMonsters(uuid, payload.monsterId, payload.monsterLevel);
-  return { status: 'success', message: 'create Monster' };
-};
+  sendGameSync(uuid, {
+    packetType: PacketType.C2S_SPAWN_MONSTER,
+    monsterId: payload.monsterId,
+    monsterLevel: payload.monsterLevel,
+  });
+
+  return { status: 'success', message: 'Monster created' };
+}
 
 // 적군 몬스터 생성
-export const EnemySpawnMonster = (uuid, payload) => {
+function handleEnemySpawnMonster(uuid, payload) {
   const monsters = createMonsters(uuid);
   if (!monsters) {
     return { status: 'fail', message: 'Monsters not found' };
   }
 
   createMonsters(uuid, payload.monsterId, payload.monsterLevel);
-  return { status: 'success', message: 'create Monster' };
+  sendGameSync(uuid, {
+    packetType: PacketType.S2C_ENEMY_SPAWN_MONSTER,
+    monsterId: payload.monsterId,
+    monsterLevel: payload.monsterLevel,
+  });
+
+  return { status: 'success', message: 'Monster created' };
+}
+
+module.exports = {
+  handleDieMonster,
+  handleEnemyDieMonster,
+  handleSpawnMonster,
+  handleEnemySpawnMonster,
 };
