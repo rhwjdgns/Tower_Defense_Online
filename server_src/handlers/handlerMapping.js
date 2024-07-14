a80b79f34bf913e303f8caebc487f358d4999aa9
yoojiwon <loveujjiwon@gmail.com>
2024-07-15 05:12:51 +0900

    🎉 add : 회원가입 로그인 기능 추가 🔥 del : 라우터 삭제, 회원가입 핸들러 파일명 통일

--- server_src/handlers/handlerMapping.js
+++ server_src/handlers/handlerMapping.js
@@ -1,12 +1,14 @@
-const { PacketType } = require('../constants');
-const { handleLogin, handleGameEnd, handleGameSync, handleGameOver } = require('./handlers');
+import { PacketType } from '../constants';
+import { handleLogin, handleRegister } from './registerHandler';
+import { handleGameEnd, someGameFunction } from './gameHandler';
+import { sendGameSync } from './gameSyncHandler';
 
 const handlerMapping = {
   [PacketType.C2S_LOGIN_REQUEST]: handleLogin,
+  [PacketType.C2S_REGISTER_REQUEST]: handleRegister,
   [PacketType.C2S_GAME_END_REQUEST]: handleGameEnd,
-  [PacketType.C2S_GAMESYNC_REQUEST]: handleGameSync,
-  [PacketType.S2C_GAME_OVER_NOTIFICATION]: handleGameOver
-  // 추가 핸들러 매핑
+  [PacketType.C2S_GAMESYNC_REQUEST]: sendGameSync,
+  // 다른 핸들러 매핑 여기에 추가해주세용!
 };
 
-module.exports = handlerMapping;
+export default handlerMapping;
--- server_src/handlers/helper.js
+++ server_src/handlers/helper.js
@@ -1,23 +1,17 @@
-//import { getUsers, removeUser } from '../models/user.model.js';
-import { CLIENT_VERSION } from '../constants.js';
-import handlerMappings from './handlerMapping.js';
+import { CLIENT_VERSION } from '../constants';
+import handlerMappings from './handlerMapping';
 
 export const handleDisconnect = (socket, uuid) => {
-  //removeUser(socket.id); // 사용자 삭제
   console.log(`User disconnected: ${socket.id}`);
-  //console.log('Current users:', getUsers());
 };
 
 export const handleConnection = async (socket, userUUID) => {
-
   console.log(`New user connected: ${userUUID} with socket ID ${socket.id}`);
-  //console.log('Current users:', getUsers());
   socket.emit('connection', { uuid: userUUID, highScore: highScore });
 };
 
 export const handleEvent = async (io, socket, data) => {
   if (!CLIENT_VERSION.includes(data.clientVersion)) {
-    // 만약 일치하는 버전이 없다면 response 이벤트로 fail 결과를 전송합니다.
     socket.emit('response', { status: 'fail', message: 'Client version mismatch' });
     return;
   }
@@ -28,13 +22,10 @@ export const handleEvent = async (io, socket, data) => {
     return;
   }
 
-  // 적절한 핸들러에 userID 와 payload를 전달하고 결과를 받습니다.
   const response = await handler(data.userId, data.payload);
-  // 만약 결과에 broadcast (모든 유저에게 전달)이 있다면 broadcast 합니다.
   if (response.broadcast) {
     io.emit('response', response);
     return;
   }
-  // 해당 유저에게 적절한 response를 전달합니다.
   socket.emit('response', response);
 };
