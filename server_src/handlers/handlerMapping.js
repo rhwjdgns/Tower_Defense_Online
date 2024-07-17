3ad64c455dc64513f52fe897e331aafa89b01b13
yoojiwon <loveujjiwon@gmail.com>
2024-07-17 15:27:28 +0900

    머지 충돌 수정

--- server_src/handlers/handlerMapping.js
+++ server_src/handlers/handlerMapping.js
@@ -3,10 +3,10 @@
 // const { handleGameSync, handleGameOver } = require('./gameHandler');
 // const { handleMatchRequest } = require ('./matchMakingHandler');
 
+
 import { PacketType } from '../constants.js';
 import { handleRegister, handleLogin } from './registerHandler.js';
-import { handleGameEnd, handleGameSync, sendGameOver } from './gameHandler.js';
-import { handleGameEnd, handleGameSync, sendGameOver } from './gameHandler.js';
+import { handleGameEnd, handleGameSync,sendGameOver } from './gameHandler.js';
 import { handleMatchRequest } from './matchMakingHandler.js';
 import { towerAddOnHandler, towerAttackHandler } from './tower.handler.js';
 import {
@@ -19,7 +19,6 @@ import {
 const handlerMapping = {
   [PacketType.C2S_REGISTER_REQUEST]: handleRegister,
   [PacketType.C2S_LOGIN_REQUEST]: handleLogin,
-  [PacketType.C2S_REGISTER_REQUEST]: handleRegister,
   [PacketType.C2S_GAME_END_REQUEST]: handleGameEnd,
   [PacketType.S2C_GAMESYNC]: handleGameSync,
   [PacketType.S2C_GAME_OVER_NOTIFICATION]: sendGameOver,
