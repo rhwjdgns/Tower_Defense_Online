edea4e663929236aacbcbc6009b34ff9d272c805
yoojiwon <loveujjiwon@gmail.com>
2024-07-17 16:04:51 +0900

    🩹 fix : 주석 추가

--- server_src/handlers/gameHandler.js
+++ server_src/handlers/gameHandler.js
@@ -1,3 +1,4 @@
+
 import { sendGameSync } from './gameSyncHandler.js';
 import { PacketType } from '../constants.js';
 
@@ -6,7 +7,7 @@ function sendGameOver(game, isWin) {
   const packet = {
     packetType: PacketType.S2C_GAME_OVER_NOTIFICATION,
     isWin: isWin,
-    message: isWin ? 'You won!' : 'You lost!',
+    message: isWin ? "You won!" : "You lost!",
   };
 
   game.player1.socket.emit('gameOver', packet);
@@ -17,22 +18,27 @@ function sendGameOver(game, isWin) {
 function handleGameEnd(socket, packet) {
   const { userId, finalScore } = packet;
   // 유저의 최종 점수를 데이터베이스에 저장하는 로직 추가
+  // 승 패 로직 추가 // 디비 저장 로직 추가 
 
   socket.emit('gameEnd', { success: true });
 }
 
 function someGameFunction(game) {
   // 게임 로직...
-}
-// 상태 동기화 호출
-function handleGameSync(socket, data) {
-  sendGameSync(game);
-  // 게임 오버 로직 예시
+
+  }
+   // 상태 동기화 호출
+  function handleGameSync(socket,data){
+     sendGameSync(game);
+       // 게임 오버 로직 예시
   if (game.player1.baseHp <= 0) {
     sendGameOver(game, false); // player1 패배
   } else if (game.player2.baseHp <= 0) {
     sendGameOver(game, true); // player1 승리
   }
 }
+
+
+
 
 export { someGameFunction, handleGameEnd, handleGameSync, sendGameOver };
